d92b6bbe5f4413b9739b8d190313ed430be99243
John Vanderbeck <jwvanderbeck@gmail.com>
2016-06-16 11:02:01 -0700

    Initial commit

---
+++ EmptyStages/EmptystageManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace EmptyStages
+{
+    [KSPAddon (KSPAddon.Startup.EditorAny, true)]
+    public class EmptystageManager : MonoBehaviour
+    {
+        public void Start()
+        {
+            KSP.UI.Screens.StageManager.EnableDeleteEmptyStages = false;
+        }
+    }
+}
+
